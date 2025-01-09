da97f902949d3d032b4e0ded493d1035c9d30e15
Gabe Hope <ghope@g.hmc.edu>
2025-01-09 10:49:28 +0000

    First build

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
+import nbformat
+from nbconvert import WebPDFExporter
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+pagebreak = r'''<div style="page-break-after: always; visibility: hidden"> 
+\pagebreak 
+</div>'''
+
+st.title('CS152 Notebook Converter')
+uploaded_file = st.file_uploader("Choose an ipynb file to convert to PDF for submission")
+if uploaded_file is not None:
+    # To read file as bytes:
+    with st.spinner('Converting...'):
+        nb = nbformat.read(uploaded_file, 4)
+        cells = []
+        for question in range(20):
+            qstr = f'Q{question}'
+            if question > 0:
+                cells.append(nbformat.v4.new_markdown_cell(source='# ' + qstr))
+            else:
+                cells.append(nbformat.v4.new_markdown_cell(source='CS152 Submission'))
+
+            for cell in nb.cells:
+                checksource = cell.source.strip().replace(' ', '').find('#!' + qstr) == 0
+                if ('tags' in cell.metadata and qstr in cell.metadata.tags) or checksource:
+                    if checksource:
+                        cell.source = '\n'.join(cell.source.splitlines()[1:])
+                    cells.append(cell)
+                    
+            cells.append(nbformat.v4.new_markdown_cell(source=pagebreak))
+
+        output = nbformat.v4.new_notebook()
+        output.cells = cells
+        (pdf, _) = WebPDFExporter(allow_chromium_download=True).from_notebook_node(output)
+        st.download_button('Download PDF', pdf, file_name='submission.pdf') 
